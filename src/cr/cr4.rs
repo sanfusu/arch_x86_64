d2f04c78b35652a6f7a5543604031f067c506b57
sanfusu <sanfusu@outlook.com>
2021-09-10 19:31:02 +0800

    field and trait impl

--- src/cr/cr4.rs
+++ src/cr/cr4.rs
@@ -1,3 +1,5 @@
+use bits::field::{BufferReader, BufferWriter};
+
 pub struct Cr4;
 pub struct Cr4Buffer {
     data: usize,
@@ -13,6 +15,30 @@ impl Cr4 {
 impl Cr4Buffer {
     pub unsafe fn flush(&mut self) {
         crx_flush!(cr4, self.data);
+    }
+}
+
+impl BufferWriter for Cr4Buffer {
+    #[must_use = "The modified value works after flushed into register"]
+    fn write<T>(&mut self, value: T::ValueType) -> &mut Self
+    where
+        T: bits::field::Field<Self> + bits::field::FieldWriter<Self>,
+    {
+        T::write(self, value);
+        self
+    }
+}
+impl BufferReader for Cr4Buffer {
+    fn read<T: bits::field::Field<Self> + bits::field::FieldReader<Self>>(&self) -> T::ValueType {
+        T::read(self)
+    }
+
+    fn output<T: bits::field::Field<Self> + bits::field::FieldReader<Self>>(
+        &self,
+        out: &mut T::ValueType,
+    ) -> &Self {
+        *out = T::read(self);
+        self
     }
 }
 
@@ -39,14 +65,25 @@ pub mod fields {
 
     bits::fields! {
         super::Cr4Buffer [data] {
-            CET [23, rw, bool]
-        }
-    }
-    #[cfg(target_arch = "x86_64")]
-    bits::fields! {
-        super::Cr4Buffer [data] {
-            PKE     [22, rw, bool],
-            SMAP    [21, rw, bool]
+            CET         [23, rw, bool],
+            PKE         [22, rw, bool],
+            SMAP        [21, rw, bool],
+            SMEP        [20, rw, bool],
+            OSXSAVE     [18, rw, bool],
+            PCIDE       [17, rw, bool],
+            FSGSBASE    [16, rw, bool],
+            UMIP        [11, rw, bool],
+            OSXMMEXCPT  [10, rw, bool],
+            OSFXSR      [09, rw, bool],
+            PCE         [08, rw, bool],
+            PGE         [07, rw, bool],
+            MCE         [06, rw, bool],
+            PAE         [05, rw, bool],
+            PSE         [04, rw, bool],
+            DE          [03, rw, bool],
+            TSD         [02, rw, bool],
+            PVI         [01, rw, bool],
+            VME         [00, rw, bool]
         }
     }
 }
