ae98ce5d084e194913ccc552f5414e24b12eca87
sanfusu <sanfusu@outlook.com>
2021-09-10 19:31:13 +0800

    fix

--- src/cr.rs
+++ src/cr.rs
@@ -1,19 +1,63 @@
+/// 这里没办法将 cr0 变成 metvariable，因为没办法在字符串里面做替换。
+/// 而 stringify 宏生成的不是字符串字面量。
 macro_rules! crx_buffer {
-    ($x:ident, $data:ident) => {
+    (cr0, $data:ident) => {
         #[cfg(target_arch = "x86")]
-        asm!("mov {:e}, $x", out(reg) $data);
+        asm!("mov {:e}, cr0", out(reg) $data);
 
         #[cfg(target_arch = "x86_64")]
-        asm!("mov {:r}, $x", out(reg) $data);
+        asm!("mov {:r}, cr0", out(reg) $data);
+    };
+    (cr2, $data:ident) => {
+        #[cfg(target_arch = "x86")]
+        asm!("mov {:e}, cr2", out(reg) $data);
+
+        #[cfg(target_arch = "x86_64")]
+        asm!("mov {:r}, cr2", out(reg) $data);
+    };
+    (cr3, $data:ident) => {
+        #[cfg(target_arch = "x86")]
+        asm!("mov {:e}, cr3", out(reg) $data);
+
+        #[cfg(target_arch = "x86_64")]
+        asm!("mov {:r}, cr3", out(reg) $data);
+    };
+    (cr4, $data:ident) => {
+        #[cfg(target_arch = "x86")]
+        asm!("mov {:e}, cr4", out(reg) $data);
+
+        #[cfg(target_arch = "x86_64")]
+        asm!("mov {:r}, cr4", out(reg) $data);
     };
 }
 macro_rules! crx_flush {
-    ($x:ident, $data:expr) => {
+    (cr0, $data:expr) => {
         #[cfg(target_arch = "x86")]
-        asm!("mov $x, {:e}", in(reg) $data);
+        asm!("mov cr0, {:e}", in(reg) $data);
 
         #[cfg(target_arch = "x86_64")]
-        asm!("mov $x, {:r}", in(reg) $data);
+        asm!("mov cr0, {:r}", in(reg) $data);
+    };
+    (cr2, $data:expr) => {
+        #[cfg(target_arch = "x86")]
+        asm!("mov cr2, {:e}", in(reg) $data);
+
+        #[cfg(target_arch = "x86_64")]
+        asm!("mov cr2, {:r}", in(reg) $data);
+    };
+    (cr3, $data:expr) => {
+        #[cfg(target_arch = "x86")]
+        asm!("mov cr3, {:e}", in(reg) $data);
+
+        #[cfg(target_arch = "x86_64")]
+        asm!("mov cr3, {:r}", in(reg) $data);
+    };
+    (cr4, $data:expr) => {
+        #[cfg(target_arch = "x86")]
+        asm!("mov cr4, {:e}", in(reg) $data);
+
+        #[cfg(target_arch = "x86_64")]
+        asm!("mov cr4, {:r}", in(reg) $data);
     };
 }
 
