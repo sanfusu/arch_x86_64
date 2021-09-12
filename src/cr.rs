0363a2267b49ade6da2b36bec79ad0c25c700210
sanfusu <sanfusu@outlook.com>
2021-09-13 03:39:48 +0800

    cr8

--- src/cr.rs
+++ src/cr.rs
@@ -2,4 +2,6 @@ pub mod cr0;
 pub mod cr2;
 pub mod cr3;
 pub mod cr4;
+#[cfg(target_arch = "x86_64")]
+pub mod cr8;
 pub mod flags;
---
+++ src/cr/cr8.rs
@@ -0,0 +1,31 @@
+pub struct Cr8;
+
+impl Cr8 {
+    #[inline]
+    pub unsafe fn buffer() -> Cr8Buffer {
+        let mut x;
+        asm!("mov {}, cr8", out(reg) x);
+        Cr8Buffer { data: x }
+    }
+}
+
+pub struct Cr8Buffer {
+    data: usize,
+}
+
+/// 用于缓存 CR8 控制寄存器；
+impl Cr8Buffer {
+    #[inline]
+    pub unsafe fn flush(&mut self) {
+        asm!("mov cr8, {}", in(reg) self.data);
+    }
+}
+impl_buffer_trait!(Cr8Buffer);
+
+pub mod fields {
+    bits::fields_ex! {
+        super::Cr8Buffer [data] {
+            pub TRR [0..=3, rw, u8]
+        }
+    }
+}
